13f96fbb63fabb9e1aa33f59a63786508ec6a8a9
razorbilldev <najimovm11@gmail.com>
2025-09-09 09:42:07 +0500

    solved #8 task

--- src/Solution.java
+++ src/Solution.java
@@ -149,4 +149,21 @@ public class Solution {
         }
         return answer;
     }
+
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int i : nums) {
+            if (i <= first) {
+                first = i;
+            } else if (i <= second) {
+                second = i;
+            }else {
+                return true;
+            }
+        }
+        return false;
+
+    }
 }
